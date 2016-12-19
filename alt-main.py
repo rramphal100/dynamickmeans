549ba0b8cd9af204cfa0f17143ef71284068b048
Ryan Ramphal <rramphal94@gmail.com>
2016-12-19 02:18:34 -0500

    Code to run the clustering algorithm and attempt to determine any possible patterns in the data.

---
+++ alt-main.py
@@ -0,0 +1,38 @@
+from sklearn.cluster import KMeans
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import datasets
+import random as rand
+from datetime import datetime
+
+dataSet = []
+maxValues = []
+
+# Read data input, get the max values to normalize
+with open("dow_jones_index.data", "r") as file:
+	columns = len(file.readline().split(",")) # Ignore first row
+	maxValues = [0] * columns
+	for line in file:
+		lineList = line.split(",")[3:-1]
+		lineList.append(line.split(",")[-1][:-2]) # Last column has trailing \r\n
+		if '' not in lineList:
+			lineList = [abs(float(attr)) for attr in lineList] # Convert everything to positive float
+			maxValues = [max(maxVal, dataVal) for maxVal, dataVal in zip(maxValues, lineList)]
+
+# Read data input, initialize data set
+with open("dow_jones_index.data", "r") as file:
+	file.readline() # Ignore first row
+	for line in file:
+		lineList = line.split(",")[3:-1]
+		lineList.append(line.split(",")[-1][:-2]) # Last column has trailing \r\n
+		if '' not in lineList: # Ignore data row with missing values
+			lineList = [float(i) for i in lineList] # Convert strings to float
+			lineList = [dataVal / maxVal for dataVal, maxVal in zip(lineList, maxValues)] # Normalize by dividing by max value
+			dataSet.append(lineList)
+
+dataArray = np.array(dataSet)
+kmeans = KMeans(n_clusters=4, random_state=0).fit(dataArray)
+
+# This will print the cluster number that each data point from dataSet list belongs to
+print kmeans.labels_
