40d7043e525ace9ba6347e8741b851eed4ac66ce
Ryan Ramphal <rramphal94@gmail.com>
2016-12-19 08:47:18 -0500

    Added printing of average data values and prediction of mapping a fictictious data points to a cluster.

--- alt-main.py
+++ alt-main.py
@@ -7,7 +7,9 @@ import random as rand
 from datetime import datetime
 
 dataSet = []
+stockSet = []
 maxValues = []
+
 
 # Read data input, get the max values to normalize
 with open("dow_jones_index.data", "r") as file:
@@ -31,8 +33,56 @@ with open("dow_jones_index.data", "r") as file:
 			lineList = [dataVal / maxVal for dataVal, maxVal in zip(lineList, maxValues)] # Normalize by dividing by max value
 			dataSet.append(lineList)
 
+with open("dow_jones_index.data", "r") as file:
+	file.readline() # Ignore first row
+	counter = 0
+	for line in file:
+		stockName = line.split(",")[1]
+		if '' not in lineList:
+			stockSet.append(stockName)
+			counter += 1
+
 dataArray = np.array(dataSet)
 kmeans = KMeans(n_clusters=4, random_state=0).fit(dataArray)
 
 # This will print the cluster number that each data point from dataSet list belongs to
 print kmeans.labels_
+
+for i in range(len(kmeans.labels_)):
+	print stockSet[i], ": ", kmeans.labels_[i]
+
+print kmeans.cluster_centers_
+
+for i in range(len(dataSet[0])):
+	avg = 0
+	for j in range(len(dataSet)):
+		avg = avg + dataSet[j][i]
+	print avg / len(dataSet), ", ",
+
+print '\n'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
